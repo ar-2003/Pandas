b29eac4b254a3fea9349bf56c0d3e5ac718a71f7
ar-2003 <aaradhyar2003@gmail.com>
2024-06-07 22:37:24 +0530

    pandas initialization

--- 1.py
+++ 1.py
@@ -1,4 +1,24 @@
-print("Hello World")
+import numpy as np
+import pandas as pd
+
+'''dict1 = {
+    "name":['harry','rohan','ar','sg'],
+    "marks": [92, 34, 24, 17],
+    "city": ['Mum', 'Delhi', 'Blr', 'Hyd']
+}
+'''
+#df = pd.DataFrame(dict1)
 
 
+#df.to_csv('friends.csv') #Exporting values to csv with filename with index
+#df.to_csv('friends_if.csv', index = False) #Exporting values to csv with filename without index
 
+#print(df) # print whole dataset
+#print(df.head(2)) # print only top 2 rows in dataset
+#print(df.tail(2)) # print only bottom 2 rows in dataset
+#print(df.describe())  # calculates 1)count 2)mean 3)std 4min 5)25% 6)50% 7)75% 8)max   
+
+temp = pd.read_csv('Temp.csv')
+#print(temp)
+#temp['Speed'][0] = 50
+print(temp['Speed'])
